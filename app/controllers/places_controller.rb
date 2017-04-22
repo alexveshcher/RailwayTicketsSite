0d11d09c6b46463de5082d6c0c8b2418823c5527
Oleh Yanivskyy <oleh_01@mail.ru>
2017-04-22 14:42:09 +0300

    Add cities selection

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -5,7 +5,13 @@ class PlacesController < ApplicationController
     p_name = params[:name]
     ukrRailway = UkrRailway.new
 
+    formatted_stations = []
+
     stations = ukrRailway.get_stations(p_name)
-    render json: stations
+
+    stations.each do |station|
+      formatted_stations << {:id => station['value'], :label => station['label']}
+    end
+    render json: formatted_stations
   end
 end
